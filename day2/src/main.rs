c4c2c9787a06d09239d63d6e1f3a7938aae85891
Stuart Roebuck <stuart.roebuck@gmail.com>
2018-12-07 17:38:06 +0000

    Day 2 part 2

--- day2/src/main.rs
+++ day2/src/main.rs
@@ -5,14 +5,19 @@ use std::io::prelude::*;
 use std::io::BufReader;
 
 fn main() -> io::Result<()> {
+    part_one()?;
+    part_two()?;
+    Ok(())
+}
+
+fn part_one() -> io::Result<()> {
     let f = File::open("input.txt")?;
     let reader = BufReader::new(f);
 
     let mut twos_count = 0;
     let mut threes_count = 0;
     for line in reader.lines() {
-        let l = line?;
-        let (twos, threes) = has_twos_and_threes(&l);
+        let (twos, threes) = has_twos_and_threes(&line?);
         if twos {
             twos_count += 1
         };
@@ -29,10 +34,36 @@ fn main() -> io::Result<()> {
     Ok(())
 }
 
+fn part_two() -> io::Result<()> {
+    let f = File::open("input.txt")?;
+    let reader = BufReader::new(f);
+
+    let lines: Vec<String> = reader.lines().map(|e| e.unwrap()).collect();
+    for (i, line) in lines.iter().enumerate() {
+        for other_line in lines[i + 1..].iter() {
+            let same = same_chars(line, other_line);
+            if same.len() == line.len() - 1 {
+                println!("{}", same);
+            }
+        }
+    }
+
+    Ok(())
+}
+
 fn has_twos_and_threes(a: &str) -> (bool, bool) {
     let mut map = HashMap::new();
     for c in a.chars() {
         map.entry(c).and_modify(|e| *e += 1).or_insert(1);
     }
     (map.values().any(|&x| x == 2), map.values().any(|&x| x == 3))
+}
+
+fn same_chars(line1: &str, line2: &str) -> String {
+    line1
+        .chars()
+        .zip(line2.chars())
+        .filter(|(x, y)| x == y)
+        .map(|(x, _)| x)
+        .collect()
 }
