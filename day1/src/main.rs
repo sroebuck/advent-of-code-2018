e56ad052ad3a1133f5271704c95dcf4de878fe2d
Stuart Roebuck <stuart.roebuck@gmail.com>
2018-12-07 19:33:53 +0000

    Day 1 part 2

--- day1/src/main.rs
+++ day1/src/main.rs
@@ -1,9 +1,16 @@
+use std::collections::HashSet;
 use std::fs::File;
 use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
 
 fn main() -> io::Result<()> {
+    part_one()?;
+    part_two()?;
+    Ok(())
+}
+
+fn part_one() -> io::Result<()> {
     let f = File::open("input.txt")?;
     let reader = BufReader::new(f);
 
@@ -12,5 +19,27 @@ fn main() -> io::Result<()> {
         count += line?.parse::<i32>().unwrap();
     }
     println!("{}", count);
+    Ok(())
+}
+
+fn part_two() -> io::Result<()> {
+    let f = File::open("input.txt")?;
+    let reader = BufReader::new(f);
+    let numbers: Vec<i32> = reader
+        .lines()
+        .map(|e| e.unwrap().parse::<i32>().unwrap())
+        .collect();
+    let mut freq_reached_set = HashSet::<i32>::new();
+    freq_reached_set.insert(0);
+    let mut freq = 0;
+    for i in (0..numbers.len()).cycle() {
+        freq += numbers[i];
+        if freq_reached_set.contains(&freq) {
+            break;
+        }
+        freq_reached_set.insert(freq);
+    }
+
+    println!("{}", freq);
     Ok(())
 }
