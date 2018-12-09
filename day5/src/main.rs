49c9bdd8f00ea81d956ac887e9ff4c8885c920f8
Stuart Roebuck <stuart.roebuck@gmail.com>
2018-12-09 20:17:55 +0000

    Day 5 part 1 refactored just because!

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -1,30 +1,56 @@
+use crate::polymer::Polymer;
 use std::fs::File;
 use std::io;
 use std::io::prelude::*;
 use std::io::BufReader;
 
-const CHAR_DIFF: i8 = 'a' as i8 - 'A' as i8;
-
 fn main() -> io::Result<()> {
     let f = File::open("input.txt")?;
     let reader = BufReader::new(f);
 
-    let mut polymer: Vec<u8> = reader.lines().next().unwrap()?.as_bytes().to_vec();
+    let mut polymer = Polymer::from_str(&reader.lines().next().unwrap()?);
+    polymer.react();
 
-    let mut i = 1;
-    while i < polymer.len() {
-        if (polymer[i - 1] as i8 - polymer[i] as i8).abs() == CHAR_DIFF {
-            polymer.remove(i - 1);
-            polymer.remove(i - 1);
-            if i > 1 {
-                i -= 1;
+    println!("Length = {}", polymer.units());
+
+    Ok(())
+}
+
+// Module added to encapsulate implementational const with Polymer definition
+
+mod polymer {
+
+    const CHAR_DIFF: i8 = 'a' as i8 - 'A' as i8;
+
+    pub struct Polymer {
+        sequence: Vec<u8>,
+    }
+
+    impl Polymer {
+        pub fn from_str(s: &str) -> Polymer {
+            Polymer {
+                sequence: s.as_bytes().to_vec(),
             }
-        } else {
-            i += 1;
+        }
+
+        pub fn react(&mut self) {
+            let mut i = 1;
+            while i < self.sequence.len() {
+                if (self.sequence[i - 1] as i8 - self.sequence[i] as i8).abs() == CHAR_DIFF {
+                    self.sequence.remove(i - 1);
+                    self.sequence.remove(i - 1);
+                    if i > 1 {
+                        i -= 1;
+                    }
+                } else {
+                    i += 1;
+                }
+            }
+        }
+
+        pub fn units(&self) -> usize {
+            self.sequence.len()
         }
     }
 
-    println!("Length = {}", polymer.len());
-
-    Ok(())
 }
