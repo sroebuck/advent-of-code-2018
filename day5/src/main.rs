acd0b5f88cf631ed8365c7812a2cbda8c6426cb4
Stuart Roebuck <stuart.roebuck@gmail.com>
2018-12-09 20:47:15 +0000

    Day 5 part 2

--- day5/src/main.rs
+++ day5/src/main.rs
@@ -11,7 +11,25 @@ fn main() -> io::Result<()> {
     let mut polymer = Polymer::from_str(&reader.lines().next().unwrap()?);
     polymer.react();
 
-    println!("Length = {}", polymer.units());
+    let polymer_units = polymer.units();
+    let mut best_units = polymer_units;
+    let mut best_unit_type_to_remove = b'.';
+    for c in polymer.unit_types() {
+        let mut p = polymer.clone();
+        p.remove_units_of_type(c);
+        p.react();
+        let pu = p.units();
+        if pu < best_units {
+            best_units = pu;
+            best_unit_type_to_remove = c;
+        }
+    }
+
+    println!("Length = {}", polymer_units);
+    println!(
+        "If you remove '{}' you get the length {}",
+        best_unit_type_to_remove as char, best_units
+    );
 
     Ok(())
 }
@@ -20,8 +38,11 @@ fn main() -> io::Result<()> {
 
 mod polymer {
 
-    const CHAR_DIFF: i8 = 'a' as i8 - 'A' as i8;
+    use std::collections::HashSet;
 
+    const CHAR_DIFF: u8 = b'a' - b'A';
+
+    #[derive(Clone)]
     pub struct Polymer {
         sequence: Vec<u8>,
     }
@@ -36,7 +57,7 @@ mod polymer {
         pub fn react(&mut self) {
             let mut i = 1;
             while i < self.sequence.len() {
-                if (self.sequence[i - 1] as i8 - self.sequence[i] as i8).abs() == CHAR_DIFF {
+                if (self.sequence[i - 1] as i8 - self.sequence[i] as i8).abs() as u8 == CHAR_DIFF {
                     self.sequence.remove(i - 1);
                     self.sequence.remove(i - 1);
                     if i > 1 {
@@ -50,6 +71,28 @@ mod polymer {
 
         pub fn units(&self) -> usize {
             self.sequence.len()
+        }
+
+        pub fn unit_types(&self) -> HashSet<u8> {
+            self.sequence
+                .iter()
+                .map(|&c| if c > b'Z' { c - CHAR_DIFF } else { c })
+                .collect()
+        }
+
+        pub fn remove_units_of_type(&mut self, c: u8) {
+            let cchar = c as char;
+            let upper_char = cchar.to_uppercase().next().unwrap() as u8;
+            let lower_char = cchar.to_lowercase().next().unwrap() as u8;
+            let mut i = 0;
+            while i < self.sequence.len() {
+                let d = self.sequence[i];
+                if d == upper_char || d == lower_char {
+                    self.sequence.remove(i);
+                } else {
+                    i += 1;
+                }
+            }
         }
     }
 
